625b2f4420c78a84e7da17cb650bcd77f4e84ac7
gcordner <geoffcordner@gmail.com>
2025-06-29 16:07:04 -0700

    frost-child front page template

---
+++ themes/alladiyally/patterns/header-alla.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Header Alla.
+ * Slug: alladiyally/header-alla
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px","left":"30px","right":"30px"},"margin":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide"><!-- wp:site-logo {"width":225,"shouldSyncIcon":true} /-->
+
+<!-- wp:navigation {"ref":4,"layout":{"type":"flex","setCascadingProperties":true}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
